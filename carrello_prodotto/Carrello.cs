cf9077337677c5c3206a6084fb59dc5c82f3869b
ghilardi.19320 <ghilardi.19320@LABI1-6PC12>
2022-11-21 09:09:37 +0100

    Aggiungere i file di progetto.

---
+++ carrello_prodotto/Carrello.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carrello_prodotto
+{
+     public class Carrello
+     {
+        private string _id;
+        private Prodotto[] prodotti = new Prodotto[100];
+        private int counter;
+
+        public Carrello(string id)
+        {
+            Id = id;
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public void Aggiungi(Prodotto p)
+        {
+            p.Id = prodotti[counter].Id;
+            counter++;
+        }
+        public void Rimuovi(Prodotto p)
+        {
+            p.Id = prodotti[counter].Id;
+            counter--;
+        }
+        public int getProdotti()
+        {
+            return counter;
+        }
+        public void Svuota()
+        {
+            for (int i = 0; i < counter; i++)
+                prodotti[i] = null;
+            counter = 0;
+        }
+
+    }
+}
---
+++ carrello_prodotto/Prodotto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carrello_prodotto
+{
+     public class Prodotto
+     {
+        private string _id, _nome, _produttore, _descrizione;
+        private float _prezzo;
+
+        public Prodotto(string id, string nome, string produttore, string descrizione)
+        {
+            Id = id;
+            Nome = nome;
+            Produttore = produttore;
+            Descrizione = descrizione;
+        }
+
+        public string Id
+        {
+            get {
+                return _id; 
+            }
+            set { 
+                _id = value; 
+            }
+        }
+        public string Nome
+        {
+            get { 
+                return _nome; 
+            }
+            set { 
+                _nome = value; 
+            }
+        }
+        public string Produttore
+        {
+            get { 
+                return _produttore; 
+            }
+            set { 
+                _produttore = value; 
+            }
+        }
+        public string Descrizione
+        {
+            get { 
+                return _descrizione; 
+            }
+            set { 
+                _descrizione = value; 
+            }
+        }
+
+        public void setPrezzo(float prezzo)
+        {
+            this._prezzo = prezzo;
+        }
+    }
+}
+
